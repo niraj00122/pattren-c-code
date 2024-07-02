d10ea08795049e603b31e93fba8ce5515f72a735
niraj00122 <ny074883@gmail.com>
2024-07-02 22:29:06 +0530

    insialize

---
+++ code/main.cpp
@@ -0,0 +1,362 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // int n;
+    // cin>>n; //input ke liye 
+    // int  i =1;
+    // while(i<=n){
+    //     int j=1;
+    //     while(j<=n){
+    //         cout<<"*";
+    //         j=j+1;
+    //     }
+    //     cout<<endl;
+    //     i=i+1;
+        
+    // }
+    
+    
+     //Q1
+     //1 2 3 4
+      //1 2 3 4
+      //1 2 3 4
+       //1 2 3 4
+      
+    //   int n;
+    //   cout<<"Enter the number";
+    //   cin>>n;
+      
+    //   int i=1;
+    //   while(i<=n){
+    //      int  j=1;
+    //       while(j<=n){
+    //           cout<<j;
+    //           j=j+1;
+    //       }
+    //       cout<<endl;
+    //       i=i+1;
+    //   }
+    
+    //Q3   
+    // 5 4 3 2 1  
+    // 5 4 3 2 1  
+    // 5 4 3 2 1  
+    // 5 4 3 2 1  
+    
+    // int n;
+    // cout<<"Enter the number";
+    // cin>>n;
+    // int i=1;
+    // while(i<=n){
+    //     int j=1;
+    //     while(j<=n){
+    //         cout<<n-j+1;
+    //         j=j+1;
+    //     }
+    //     cout<<endl;
+    //     i=i+1;
+    // }
+    
+    
+    // Q4
+    // 123
+    // 456
+    // 789
+    
+    // int n;
+    // cout<<"Enter the value";
+    // cin>>n;
+    // int i=1;
+    // int count =1;
+    // while(i<=n){
+    //     int j=1;
+    //     while(j<=n){
+    //         cout<<count<<" ";
+    //         count=count+1;
+    //       j=j+1;
+    //     }
+    //     cout<<endl;
+    //     i=i+1;
+    // }
+    
+    // Q4
+    // *
+    // **
+    // ***
+    // ****
+    // *****
+    // int n;
+    // cout<<"Enter the number ";
+    // cin>>n;
+    // int row=1;
+    // while(row<=n){
+    //     int colb=1;
+    //     while(colb<=row){
+    //         cout<<"*";
+    //  colb=colb+1;
+    //     }
+    //     cout<<endl;
+    //   row=row+1;
+    // }
+    
+    
+    
+    // Q5
+    // 1
+    // 12
+    // 123
+    // 1234
+    
+    // int n;
+    // cout<<"Enter the number";
+    // cin>>n;
+    // int row=1;
+    // while(row<=n){
+    //     int col=1;
+    //     while(col<=row){
+    //         cout<<row;
+    //         col=col+1;
+    //     }
+    //     cout<<endl;
+    //     row=row+1;
+    // }
+    
+    
+    // Q6
+    // 1
+    // 23
+    // 456
+    // 78910
+    
+    // int n;
+    // cout<<"Enter the number";
+    // cin>>n;
+    // int row=1;
+    // int count=1;
+    // while(row<=n){
+    //     int col=2;
+    //     while(col<=row){
+    //         cout<<count<<" ";
+    //         count=count+1;
+    //         col=col+1;
+    //     }
+    //     cout<<endl;
+    //     row=row+1;
+    // }
+    
+
+    //  Q7
+    // 1
+    // 23
+    // 345
+    // 4567
+    // 56789
+    // int n; 
+    // cin>>n;
+    // int row=1;
+    // while(row<=n){
+    //     int col =1;
+    //     int value=row;
+    //     while(col<=row){
+    //     cout<<value;
+    //     value=value+1;
+        
+    //         col=col+1;
+            
+    //     }
+    //     cout<<endl;
+    //     row=row+1;
+    // }
+    
+    // 2nd method same code;
+    // int n;
+    // cout<<"Enter the number";
+    // cin>>n;
+    
+    // int row=1;
+    // while(row<=n){
+    //     int col=1;
+    //     while(col<=row){
+    //         cout<<row+col;
+    //         col=col+1;
+    //     }
+    //     cout<<endl;
+    //     row=row+1;
+        
+    // }
+    
+    
+    
+//     Q8
+//     1
+//     2 1
+//     3 2 1
+//     4 3 2 1
+//      code
+
+// int n;
+// cout<<"Enter the number";
+// cin>>n;
+// int row=1;
+// while(row<=n){
+//     int col=1;
+//     while(col<=row){
+//         cout<<row-col+1;
+//         col=col+1;
+//     }
+//     cout<<endl;
+//     row=row+1;
+
+
+
+    // Q9
+    // AAAA
+    // BBBB
+    // CCCC
+    // DDDD
+    
+    //  int n;
+    //  cin>>n;
+    //  int row=1;
+    //  while(row<=n){
+    //      int col=1;
+    //      while(col<=n){
+    //          char st='A'+row-1;
+    //          cout<<st;
+    //          col=col+1;
+    //      }
+    //      cout<<endl;
+    //      row=row+1;
+    //  }
+         
+    //  }
+    
+    
+    // Q10
+    // ABC
+    // BCD
+    // CDF
+    // int n;
+    // cin>>n;
+    // int i=1;
+    // while(i<=n){
+    //     int j=1;
+    //     while(j<=n){
+    //         char ch='A'+j-1;
+    //         cout<<ch;
+    //         j=j+1;
+    // }
+    // cout<<endl;
+    // i=i+1;
+    // }
+    
+    // Q11
+    // ABC
+    // DEF
+    // GHI
+    
+//   int n;
+//     cout<<"Enter the value";
+//     cin>>n;
+//     int i=1;
+//     char ch='A';
+//     while(i<=n){
+//         int j=1;
+//         while(j<=n){
+//             cout<<ch<<" ";
+//             ch=ch+1;
+//           j=j+1;
+//         }
+//         cout<<endl;
+//         i=i+1;
+//     }
+
+// Q12
+// ABCD
+// EFGH
+// UJKL
+
+// int n;
+// cin>>n;
+// int row=1;
+// while(row<=n){
+//     int col=1;
+//     while(col<=n){
+//         char ch='A'+row+col-2;
+//         cout<<ch;
+//         col=col+1;
+//     }
+//     cout<<endl;
+//     row=row+1;
+// }
+
+
+// Q13
+// A
+// BB
+// CCCC
+// DDDDD
+// int n;
+// cin>>n;
+// int row=1;
+// while(row<=n){
+//     int col=1;
+//     while(col<=row){
+//         char ch='A'+row-1;
+//         cout<<ch;
+//         col=col+1;
+//     }
+//     cout<<endl;
+//     row=row+1;
+// }
+
+// Q14.
+// A
+// BC
+// CDF
+// DFGH
+// int n;
+// cin>>n;
+// int row=1;
+// while(row<=n){
+//     int col=1;
+//     while(col<=row){
+//         char ch=('A'+row+col-1);
+//         cout<<ch;
+//         col=col+1;
+//     }
+//     cout<<endl;
+//     row=row+1;
+// }
+
+
+
+int n;
+cin>>n;
+int row=1;
+while(row<=n){
+    int col=1;
+     char star='A'+n-1;
+    while(col<=row){
+       
+        cout<<star;
+        star=star+1;
+        col=col+1;
+    }
+    cout<<endl;
+    row=row+1;
+}
+
+
+
+}
+
+
+
+    
+    
+    
+    
+    
+    
+    
